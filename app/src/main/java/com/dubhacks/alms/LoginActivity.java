236ebb6b927989ba0d9a471e53baf168f0f01abe
Tuan Nguyen <nguyen.tuan@husky.neu.edu>
2020-10-17 23:44:40 -0700

    update MapActivity w ListView

--- app/src/main/java/com/dubhacks/alms/LoginActivity.java
+++ app/src/main/java/com/dubhacks/alms/LoginActivity.java
@@ -93,4 +93,9 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(intent);
     }
+
+    @Override
+    public void onBackPressed() {
+//         super.onBackPressed();
+    }
 }
